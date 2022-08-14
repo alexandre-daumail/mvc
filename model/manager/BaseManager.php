99bfc100ca88fcae0d1a6454643213f6379b054b
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-08-14 15:03:25 +0200

    BaseManager.php added

---
+++ model/manager/BaseManager.php
@@ -0,0 +1,36 @@
+<?php
+	class BaseManager
+	{
+		private $_table;
+		private $_object;
+		
+		public function __construct()
+		{
+			
+		}
+		
+		public function getById($id)
+		{
+			
+		}
+		
+		public function getAll()
+		{
+			
+		}
+		
+		public function create($obj)
+		{
+			
+		}
+		
+		public function update($obj)
+		{
+			
+		}
+		
+		public function delete($obj)
+		{
+			
+		}
+	}
