3732e88a2a2d114ee0ed59618baa3052e3e70095
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-08-14 14:48:51 +0200

    ViewNotFoundException added

---
+++ framework/exception/ViewNotFoundException.php
@@ -0,0 +1,8 @@
+<?php
+class ViewNotFoundException extends Exception
+{
+    public function __construct($message = "Aucune vue n'a été trouvée")
+    {
+        parent::__construct($message, "0003");
+    }
+}
