cf95d8202e8e7cda5ee8c6cb7cb1f096934265ba
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-08-14 15:11:04 +0200

    BanseManager method added

--- model/manager/BaseManager.php
+++ model/manager/BaseManager.php
@@ -3,6 +3,7 @@
 	{
 		private $_table;
 		private $_object;
+        protected $_bdd;
 		
 		public function __construct($table,$object,$datasource)
 		{
@@ -13,12 +14,18 @@
 		
 		public function getById($id)
 		{
-			
+			$req = $_bdd->prepare("SELECT * FROM " . $this->_table . " WHERE id=?");
+			$req->execute(array($id));
+			$req->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,$this->_obj);
+			return $req->fetch();
 		}
 		
 		public function getAll()
 		{
-			
+			$req = $_bdd->prepare("SELECT * FROM " . $this->_table);
+			$req->execute();
+			$req->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,$this->_obj);
+			return $req->fetchAll();
 		}
 		
 		public function create($obj)
@@ -33,6 +40,14 @@
 		
 		public function delete($obj)
 		{
-			
+			if(property_exists($obj,"id"))
+			{
+				$req = $_bdd->prepare("DELETE FROM " . $this->_table . " WHERE id=?");
+				return $req->execute(array($obj->id));
+			}
+			else
+			{
+				throw new PropertyNotFoundException();	
+			}
 		}
 	}
