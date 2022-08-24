f6f932e6c22b5aec4f4bc678840e7456eb2dfbbd
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-08-24 16:45:45 +0200

    Route.php update

--- framework/Route.php
+++ framework/Route.php
@@ -1,44 +1,50 @@
 <?php
-
-class Route
-{
-    private $path;
-    private $controller;
-    private $action;
-    private $method;
-    private $param;
-
-    public function __construct($route)
-    {
-        $this->path = $route->path;
-        $this->controller = $route->controller;
-        $this->action = $route->action;
-        $this->method = $route->method;
-        $this->param = $route->param;
-    }
-
-    public function getPath()
-    {
-        return $this->path;
-    }
-
-    public function getController()
-    {
-        return $this->controller;
-    }
-
-    public function getAction()
-    {
-        return $this->action;
-    }
-
-    public function getMethod()
-    {
-        return $this->method;
-    }
-
-    public function getParam()
-    {
-        return $this->param;
-    }
-}
+	class Route
+	{
+		private $_path;
+		private $_controller;
+		private $_action;
+		private $_method;
+		private $_param;
+		private $_manager;
+		
+		public function __construct($route)
+		{
+			$this->_path = $route->path;
+			$this->_controller = $route->controller;
+			$this->_action = $route->action;
+			$this->_method = $route->method;
+			$this->_param = $route->param;
+			$this->_manager = $route->manager;
+		}
+		
+		public function getPath()
+		{
+			return $this->_path;
+		}
+		
+		public function getController()
+		{
+			return $this->_controller;
+		}
+		
+		public function getAction()
+		{
+			return $this->_action;
+		}
+		
+		public function getMethod()
+		{
+			return $this->_method;
+		}
+		
+		public function getParam()
+		{
+			return $this->_param;
+		}
+		
+		public function getManager()
+		{
+			return $this->_manager;
+		}
+	}
