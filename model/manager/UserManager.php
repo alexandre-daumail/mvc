034625220bc3bd156a1aa7c776195dddb7f7e6e2
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-08-24 16:15:44 +0200

    UserManager update

--- model/manager/UserManager.php
+++ model/manager/UserManager.php
@@ -5,4 +5,12 @@
 		{
 			parent::__construct("user","User",$datasource);	
 		}
+		
+		public function getByMail($mail)
+		{
+			$req = $this->_bdd->prepare("SELECT * FROM user WHERE mail=?");
+			$req->execute(array($mail));
+			$req->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,"User");
+			return $req->fetch();
+		}
 	}
