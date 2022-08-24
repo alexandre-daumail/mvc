57207bb2e967b2ff583d5a060cb59dbcdab178ad
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-08-24 17:11:55 +0200

    baseController update

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -1,34 +1,40 @@
 <?php
-class BaseController
-{
-    private $_httpRequest;
-    private $_param;
-
-    public  function __construct($httpRequest)
-    {
-        $this->_httpRequest = $httpRequest;
-    }
-
-    protected function view($filename)
-    {
-        if (file_exists('View/' . $filename . '.php')) {
-            ob_start();
-            extract($this->_param);
-            include("View/" . $filename . ".php");
-            $content = ob_get_clean();
-            include("View/layout.php");
-        } else {
-            throw new ViewNotFoundException();
-        }
-    }
-
-    public function bindManager()
-    {
-        # code...
-    }
-
-    public function addParam($name,$value)
+	class BaseController
+	{
+		private $_httpRequest;
+		private $_param;
+		
+		public function __construct($httpRequest)
+		{
+			$this->_httpRequest = $httpRequest;
+		}
+		
+		protected function view($filename)
+		{
+			if(file_exists('View/' . $filename . '.php'))
+			{
+				ob_start();
+				extract($this->_param);
+				include("View/" . $filename . ".php");
+				$content = ob_get_clean();
+				include("View/layout.php");
+			}
+			else
+			{
+				throw new ViewNotFoundException();	
+			}
+		}
+		
+		public function bindManager()
+		{
+			foreach($this->_httpRequest->getRoute()->getManager() as $manager)
+			{
+				$this->$manager = new $manager;
+			}
+		}
+		
+		public function addParam($name,$value)
 		{
 			$this->_param[$name] = $value;
 		}
-}
+	}
