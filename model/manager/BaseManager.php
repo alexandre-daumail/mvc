7992179d592d1a26136a10ad9ae481c185a631f2
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-08-24 15:56:22 +0200

    bdd update

--- model/manager/BaseManager.php
+++ model/manager/BaseManager.php
@@ -14,7 +14,7 @@
 		
 		public function getById($id)
 		{
-			$req = $_bdd->prepare("SELECT * FROM " . $this->_table . " WHERE id=?");
+			$req = $this->_bdd->prepare("SELECT * FROM " . $this->_table . " WHERE id=?");
 			$req->execute(array($id));
 			$req->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,$this->_obj);
 			return $req->fetch();
@@ -22,7 +22,7 @@
 		
 		public function getAll()
 		{
-			$req = $_bdd->prepare("SELECT * FROM " . $this->_table);
+			$req = $this->_bdd->prepare("SELECT * FROM " . $this->_table);
 			$req->execute();
 			$req->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,$this->_obj);
 			return $req->fetchAll();
@@ -35,7 +35,7 @@
 			$valueArray = array_fill(1,$paramNumber,"?");
 			$valueString = implode(", ", $valueArray);
 			$sql = "INSERT INTO " . $this->_table . "(" . implode(", ", $param) . ") VALUES(" . $valueString . ")";
-			$req = $_bdd->prepare($sql);
+			$req = $this->_bdd->prepare($sql);
 			$boundParam = array();
 			foreach($param as $paramName)
 			{
@@ -59,7 +59,7 @@
 				$sql = $sql . $paramName . " = ?, ";
 			}
 			$sql = $sql . " WHERE id = ? ";
-			$req = $_bdd->prepare($sql);
+			$req = $this->_bdd->prepare($sql);
 			
 			$param[] = 'id';
 			$boundParam = array();
@@ -82,7 +82,7 @@
 		{
 			if(property_exists($obj,"id"))
 			{
-				$req = $_bdd->prepare("DELETE FROM " . $this->_table . " WHERE id=?");
+				$req = $this->_bdd->prepare("DELETE FROM " . $this->_table . " WHERE id=?");
 				return $req->execute(array($obj->id));
 			}
 			else
