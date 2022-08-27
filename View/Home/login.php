2466865eaf2c8ea7dcebbfde026e35766b026637
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-08-27 11:18:26 +0200

    Create login.php

---
+++ View/Home/login.php
@@ -0,0 +1,8 @@
+<?php $title = "Login" ?>
+<form class="login-form" action="Login" method="post"> 
+	<label>Identifiant</label>
+	<input type="text" name="login" />
+	<label>Mot de passe</label>
+	<input type="password" name="password" />
+	<input type="submit" value="Se connecter" />
+</form>
