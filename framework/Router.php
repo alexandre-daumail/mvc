8062a3b1f2146e864d087fca35fa3ac8a354456a
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-08-27 09:57:36 +0200

    Update Router.php

--- framework/Router.php
+++ framework/Router.php
@@ -1,34 +1,31 @@
 <?php
-    class Router
-    {
-        private $listRoute;
-
-        public function __construct()
-        {
-            $stringRoute = file_get_contents('config/route.json');
-            $this->listRoute = json_decode($stringRoute);
-        }
-
-        public function findRoute($httpRequest)
-        {
-            $routeFound = array_filter($this->listRoute, function($route) use ($httpRequest){
-                return preg_match("#^" . $route->path . "$#", $httpRequest->getUrl()) && $route->method == $httpRequest->getMethod();
-            });
-
-            $numberRoute = count($routeFound);
-
-            if ($numberRoute > 1) 
-            {
-                throw new MultipleRouteFoundException();
-            }
-            elseif ($numberRoute == 0)
-            {
-                throw new NoRouteFoundException();
-            }
-            else
-            {
-                return new Route(array_shift($routeFound));	
-            }
-        }
-    }
-    
+	class Router
+	{
+		private $_listRoute;
+		
+		public function __construct()
+		{
+			$stringRoute = file_get_contents('Config/route.json');
+			$this->_listRoute = json_decode($stringRoute);
+		}
+		
+		public function findRoute($httpRequest)
+		{
+			$routeFound = array_filter($this->_listRoute,function($route) use ($httpRequest){
+				return preg_match("#^" . $route->path . "$#", $httpRequest->getUrl()) && $route->method == $httpRequest->getMethod();
+			});
+			$numberRoute = count($routeFound);
+			if($numberRoute > 1)
+			{
+				throw new MultipleRouteFoundException();
+			}
+			else if($numberRoute == 0)
+			{
+				throw new NoRouteFoundException($httpRequest);
+			}
+			else
+			{
+				return new Route(array_shift($routeFound));	
+			}
+		}
+	}
